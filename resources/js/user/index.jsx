f39dbda156f79d5b3499d6054057d9c705187b9c
s.a <appurupei64@gmail.com>
2022-10-31 23:11:15 +0900

    ログイン時の表示変更完了

--- resources/js/user/index.jsx
+++ resources/js/user/index.jsx
@@ -1,14 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import Guest from "./navbar/guest";
+import Auth from "./navbar/auth";
+import axios from "axios";
 
 function Index() {
-    // const {getToken} = AuthUser();
-    // if(!getToken()){
-    return (<Guest />)
-    // }
-    // return (
-    //     <Auth />
-    // );
+    const [userdetail,setUserdetail] = useState('');
+
+    useEffect(()=>{
+        fetchUserDetail();
+    },[]);
+
+    const fetchUserDetail = async() =>{
+        await axios.get('/me').then((res)=>{
+            setUserdetail(res.data);
+        });
+    }
+    function renderElement(){
+        if(!userdetail){
+            return <Guest />
+        }else{
+            return <Auth />
+        }
+
+    }
+
+    return(
+        <>
+            { renderElement() }
+        </>
+    )
 
 }
 
--- resources/js/user/navbar/auth.jsx
+++ resources/js/user/navbar/auth.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../home';
+
 
 function Auth() {
+    const login = () =>{
+        window.location.replace("/login")
+    }
+    const register = () =>{
+        window.location.replace("/register")
+    }
+    return (
+    <>
+        <ul className="hidden md:flex ">
+            ログイン済み
+            <li>
+                <Link className='no-underline' to="/">Home</Link>
+            </li>
 
+        </ul>
+        <div className="">
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    </>
+    );
 }
 
 export default Auth;
