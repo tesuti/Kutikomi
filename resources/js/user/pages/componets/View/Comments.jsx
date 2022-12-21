384fc8157e50e3a7f603cf23ac36560c9289b77a
s.a <appurupei64@gmail.com>
2022-12-21 16:37:13 +0900

    o

--- resources/js/user/pages/componets/View/Comments.jsx
+++ resources/js/user/pages/componets/View/Comments.jsx
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { FaStar } from 'react-icons/fa';
 const  Comments = () => {
@@ -10,16 +10,26 @@ const  Comments = () => {
     const [editComment, setEditComment] = useState("");
     const [editRating, setEditRating] = useState("");
 
+    const [popup, setPopup] = useState("");
+    let [sidebar, setSidebar]= useState(false);
+    let [openEdit, setOpenEdit]= useState(false);
+    let [openDelete, setOpenDelete]= useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
     const {id} = useParams();
 
-
+    // const  menuRef =  useRef();
+    // const  imgRef =  useRef();
     const editData = {
         comment: editComment,
         rating: editRating,
         id:id,
     };
 
-
+    // window.addEventListener('click',(e)=>{
+    //     if(e.target !== menuRef.current && e.target !==  imgRef.current){
+    //         const showSidebar = () => setSidebar(!sidebar);
+    //     }
+    //   });
 
     const submitEdit = (id)=>{
         axios.put('/comment/'+id,editData).then((res)=>{
@@ -83,28 +93,63 @@ const  Comments = () => {
     })}
 
     <p>{ commentField.comment}</p>
+
 {userdetail.id == commentField.user_id ?
 <>
 
 <div>
-    <label>コメント</label>
-    <input type="text" name='comment' className=''
-    value={editComment || ''}
-    onChange={(e) => setEditComment(e.target.value)}
-    />
+    <div  onClick={showSidebar}>
+        <button  onClick={() =>{setPopup(commentField.id)}}>:</button>
+    </div>
+    {popup == commentField.id ?
+        <>
+        <ul   className={` top-6 cursor-pointer
+            ${sidebar ? 'hidden':' '}`}>
+        <li className='md:ml-8 text-xl md:my-0 my-7 '>
+            <div >
+                <button onClick={() => setOpenEdit(!openEdit)} className="text-gray-800 hover:text-gray-400 duration-500" >編集</button>
+            </div>
+        </li>
+        <li  onClick={() => setOpenDelete(!openDelete)} className='md:ml-8 text-xl md:my-0 my-7'>
+            <button className="text-gray-800 hover:text-gray-400 duration-500" >削除</button>
+        </li>
 
-    <label>評価</label>
-    <input type="text" name='rating' className=''
-    value={editRating || ''}
-    onChange={(e) => setEditRating(e.target.value)}
-    />
-    <button type='button' onClick={() =>{submitEdit(commentField.id)}}>編集</button>
-</div>
+    <div  className={`  -8 top-6 cursor-pointer
+                ${openEdit ? '':' hidden'}`}>
+        <label>コメント</label>
+            <input type="text" name='comment' className=''
+            value={editComment || ''}
+            onChange={(e) => setEditComment(e.target.value)}
+            />
 
-    <button type="button" className="btn"
-    onClick={()=>{deleteComment(commentField.id)}}>
-        削除
-    </button>
+        <label>評価</label>
+            <input type="text" name='rating' className=''
+            value={editRating || ''}
+            onChange={(e) => setEditRating(e.target.value)}
+            />
+
+        <div onClick={showSidebar}>
+            <button type='button' onClick={() =>{submitEdit(commentField.id)}}> 編集
+            </button>
+        </div>
+    </div>
+        <div className={` top-6 cursor-pointer
+            ${openDelete ? '':' hidden'}`}>
+                <p>コメントの削除</p>
+                <p>コメントを完全に削除しますか？</p>
+                <button onClick={showSidebar}>キャンセル</button>
+                <div onClick={showSidebar}>
+                    <button type="button" className="btn"
+                    onClick={()=>{deleteComment(commentField.id)}}>
+                        削除
+                    </button>
+            </div>
+        </div>
+    </ul>
+     </>
+        :''}
+  </div>
+
 </>
 :''
 }
